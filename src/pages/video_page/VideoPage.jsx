8679daba8375f44972cb1c5ecadb24b92024c7d5
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2022-04-06 23:33:28 +0530

    note-taking

--- src/pages/video_page/VideoPage.jsx
+++ src/pages/video_page/VideoPage.jsx
@@ -2,6 +2,7 @@ import "./video_page.css";
 import { useParams } from "react-router-dom";
 import { SingleVideoPageCard } from "../../components";
 import { useVideoState } from "../../context";
+import { NotesForm, NotesSection } from "./component";
 import { useDocumentTitle } from "../../custom_hooks";
 import { removeAllNotesForVideo } from "../../services";
 
@@ -19,6 +20,28 @@ export const VideoPage = () => {
   return (
     <main className="main center">
       <SingleVideoPageCard video={video}>
+        <section className="section notes-management">
+          <div className="flex-align-center">
+            <h2>Notes</h2>
+            <button
+              className="btn video_card-btn clear-btn"
+              title="Like"
+              onClick={() =>
+                removeAllNotesForVideo({
+                  videoId,
+                  videoStateDispatch,
+                  showToast,
+                })
+              }
+            >
+              <i className={`fa-solid fa-trash `}></i>
+              Clear Notes
+            </button>
+          </div>
+
+          <NotesForm videoId={videoId} />
+          <NotesSection videoId={videoId} />
+        </section>
       </SingleVideoPageCard>
     </main>
   );
